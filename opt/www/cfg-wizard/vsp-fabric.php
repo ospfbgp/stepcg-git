0b6cbf654cdcc07a0b6501877275c0aeee7594d2
ospfbgp <danielcoxman@gmail.com>
2018-05-17 10:50:03 -0500

    Update vsp-fabric.php

--- opt/www/cfg-wizard/vsp-fabric.php
+++ opt/www/cfg-wizard/vsp-fabric.php
@@ -8,6 +8,9 @@
 <body>  
 
 <?php
+$manualarea = "5.2018";
+$pribvlan = "4051";
+$secbvlan = "4052";
 ///https://www.w3schools.com/php/php_form_complete.asp
 // define variables and set to empty values
 $error = "";
@@ -23,14 +26,24 @@ $nickname = "";
 $nicknameErr = "";
 $isisinterfaces = "";
 $isisinterfacesErr = "";
-$manualarea = "5.2018";
-$pribvlan = "4051";
-$secbvlan = "4052";
+$vistvlan = "";
+$vistvlanErr = "";
+$vistisid = "";
+$vistvlanisidErr = "";
+$vistipaddress = "";
+$vistipaddressErr = "";
+$vistnetworkmask = "";
+$vistnetworkmaskErr = "";
+$vistpeerip = "";
+$vistpeeripErr = "";
+$smltpeersystemid = "";
+$smltpeersystemidErr = "";
 
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
 
   if (empty($_POST["sysname"])) {
     $sysnameErr = "Sys-name is required";
+    $error = 1;
   } else {
     $sysname = test_input($_POST["sysname"]);
     // check if name only contains letters and whitespace
@@ -42,18 +55,21 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
 
   if (empty($_POST["contact"])) {
     $contactErr = "SNMP Contact is required";
+    $error = 1;
   } else {
     $contact = test_input($_POST["contact"]);
   }
 
   if (empty($_POST["location"])) {
     $locationErr = "SNMP Location is required";
+    $error = 1;
   } else {
     $location = test_input($_POST["location"]);
   }
 
   if (empty($_POST["loopback1"])) {
     $loopback1Err = "Loopback 1 is required";
+    $error = 1;
   } else {
     $loopback1 = test_input($_POST["loopback1"]);
     // check if name only contains letters and whitespace
@@ -66,6 +82,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
 
   if (empty($_POST["nickname"])) {
     $nicknameErr = "Nick-name is required";
+    $error = 1;
   } else {
     $nickname = test_input($_POST["nickname"]);
     // check if name only contains letters and whitespace
@@ -77,9 +94,20 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
 
   if (empty($_POST["isisinterfaces"])) {
     $isisinterfacesErr = "ISIS Interfaces required";
+    $error = 1;
   } else {
     $isisinterfaces = test_input($_POST["isisinterfaces"]);
   }
+
+  if (!empty($_POST["vistvlan"])) {
+    $vistvlan = test_input($_POST["vistvlan"]);
+    $vistisid = test_input($_POST["vistisid"]);
+    $vistipaddress = test_input($_POST["vistipaddress"]);
+    $vistnetworkmask = test_input($_POST["vistnetworkmask"]);
+    $vistpeerip = test_input($_POST["vistpeerip"]);
+    $smltpeersystemid = test_input($_POST["smltpeersystemid"]);
+  } 
+
 }
 
 function test_input($data) {
@@ -91,7 +119,7 @@ function test_input($data) {
 ?>
 
 <h2>VSP Fabric Startup</h2>
-<p><span class="error">* required field</span></p>
+<p>Please input your fields. <span class="error">* required field</span></p>
 <form method="post" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>">  
   Sys name: <input type="text" name="sysname" value="<?php echo $sysname;?>">
   <span class="error">* <?php echo $sysnameErr;?></span>
@@ -110,13 +138,38 @@ function test_input($data) {
   <br><br>
   ISIS interfaces: <input type="text" name="isisinterfaces" value="<?php echo $isisinterfaces;?>">
   <span class="error">* <?php echo $isisinterfacesErr;?></span>
+  <h3>Virtual-Ist setup for clustered VSP switches</h3>
+  Vist VLAN: <input type="text" name="vistvlan" value="<?php echo $vistvlan;?>">
+  <span class="error"> <?php echo $vistvlanErr;?></span>
+  <br><br>
+  Vist i-sid: <input type="text" name="vistisid" value="<?php echo $vistisid;?>">
+  <span class="error"> <?php echo $vistisidErr;?></span>
+  <br><br>
+  Vist IP Address: <input type="text" name="vistipaddress" value="<?php echo $vistipaddress;?>">
+  <span class="error"> <?php echo $vistipaddressErr;?></span>
+<select name="vistnetworkmask" >
+  <option value="255.255.255.252" <?php if($vistnetworkmask == '255.255.255.252'){echo("selected");}?>>255.255.255.252</option>
+  <option value="255.255.255.248" <?php if($vistnetworkmask == '255.255.255.248'){echo("selected");}?>>255.255.255.248</option>
+  <option value="255.255.255.240" <?php if($vistnetworkmask == '255.255.255.240'){echo("selected");}?>>255.255.255.240</option>
+  <option value="255.255.255.224" <?php if($vistnetworkmask == '255.255.255.224'){echo("selected");}?>>255.255.255.224</option>
+  <option value="255.255.255.192" <?php if($vistnetworkmask == '255.255.255.192'){echo("selected");}?>>255.255.255.192</option>
+  <option value="255.255.255.128" <?php if($vistnetworkmask == '255.255.255.128'){echo("selected");}?>>255.255.255.128</option>
+  <option value="255.255.255.0" <?php if($vistnetworkmask == '255.255.255.0'){echo("selected");}?>>255.255.255.0</option>
+  <option value="255.255.254.0" <?php if($vistnetworkmask == '255.255.254.0'){echo("selected");}?>>255.255.254.0</option>
+</select>
+  <br><br>
+  Vist Peer IP Address: <input type="text" name="vistpeerip" value="<?php echo $vistpeerip;?>">
+  <span class="error"> <?php echo $vistpeeripErr;?></span>
+  <br><br>
+  SMLT Peer system-id: <input type="text" name="smltpeersystemid" value="<?php echo $smltpeersystemid;?>">
+  <span class="error"> <?php echo $smltpeersystemidErr;?></span>
   <br><br>
   <input type="submit" name="submit" value="Create Config">  
 </form>
 
 <?php
 if ($error == 0) {
-echo "<h2>VSP Fabric Configuration:</h2>";
+echo "<hr><h3>VSP Fabric Startup Configuration:</h3>";
 echo "
 enable<br>
 config t<br>
@@ -167,7 +220,35 @@ y<br>
 exit<br>
 <br>
 vlacp enable<br>
+<hr>
 ";
+
+if (!empty($_POST["vistvlan"])){
+echo "<h3>VSP Vist Configuration</h3>";
+echo "
+enable<br>
+config t<br>
+vlan create <font color=\"orange\">$vistvlan</font> name \"vist\" type port-mstprstp 1<br>
+vlan i-sid <font color=\"orange\">$vistvlan $vistisid</font><br>
+interface Vlan <font color=\"orange\">$vistvlan</font><br>
+ip address <font color=\"orange\">$vistipaddress $vistnetworkmask</font><br>
+exit<br>
+<br>
+no router isis enable<br>
+y<br>
+<br>
+virtual-ist peer-ip <font color=\"orange\">$vistpeeripaddress</font> vlan <font color=\"orange\">$vistvlan</font><br>
+router isis<br>
+spbm 1 smlt-peer-system-id <font color=\"orange\">$smltpeersystemid</font><br>
+router isis enable<br>
+<br>
+ip rsmlt edge-support<br>
+exit<br>
+<hr>
+";
+
+}
+
 }
 ?>
 
