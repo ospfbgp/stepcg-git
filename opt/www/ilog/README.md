d0bf7e88a96644e4386706532f89e1006d39ef97
ospfbgp <danielcoxman@gmail.com>
2019-03-06 13:50:33 -0500

    Add files via upload

---
+++ opt/www/ilog/README.md
@@ -0,0 +1,11 @@
+# ilog
+This uses jquery, DataTables and Bootstrap to allow you to search a mysql database with syslog messages.  The search is very fast and uses server side processing.  I built a database and table for the logs.  I then used syslog-ng to send the logs to mysql database. 
+
+To clone repository
+<pre>
+git clone https://github.com/ospfbgp/ilog
+</pre>
+
+<p align="center">
+  <img src="extra/iLog_Syslog_search_screen_1.png" width="1000"/>
+</p>
---
+++ opt/www/ilog/scripts/device_list.php
@@ -0,0 +1,65 @@
+<?php
+
+/*
+ * DataTables example server-side processing script.
+ *
+ * Please note that this script is intentionally extremely simply to show how
+ * server-side processing can be implemented, and probably shouldn't be used as
+ * the basis for a large complex system. It is suitable for simple use cases as
+ * for learning.
+ *
+ * See http://datatables.net/usage/server-side for full details on the server-
+ * side processing requirements of DataTables.
+ *
+ * @license MIT - http://datatables.net/license_mit
+ */
+
+/* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *
+ * Easy set variables
+ */
+
+// DB table to use
+$table = 'devices';
+
+// Table's primary key
+$primaryKey = 'device_id';
+
+// Array of database columns which should be read and sent back to DataTables.
+// The `db` parameter represents the column name in the database, while the `dt`
+// parameter represents the DataTables column identifier. In this case simple
+// indexes
+
+$columns = array(
+        array( 'db' => 'device_id',  'dt' => 0 ),
+        array( 'db' => 'status',     'dt' => 1 ),
+        array( 'db' => 'sysName',    'dt' => 2 ),
+        array( 'db' => 'hostname',    'dt' => 3 ),
+        array( 'db' => 'hostname',   'dt' => 4 ),
+        array( 'db' => 'location',   'dt' => 5 ),
+        array( 'db' => 'hardware',   'dt' => 6 ),
+        array( 'db' => 'os',         'dt' => 7 ),
+        array( 'db' => 'version',    'dt' => 8 ),
+        array( 'db' => 'serial',     'dt' => 9 ),
+        array( 'db' => 'features',   'dt' => 10 ),
+        array( 'db' => 'uptime',     'dt' => 11 )
+
+);
+
+// SQL server connection information
+$sql_details = array(
+        'user' => 'ilog',
+        'pass' => 'ilogpassword',
+        'db'   => 'librenms',
+        'host' => 'localhost'
+);
+
+/* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *
+ * If you just want to use the basic configuration for DataTables with PHP
+ * server-side, there is no need to edit below this line.
+ */
+
+require( 'ssp.class.php' );
+
+echo json_encode(
+        SSP::simple( $_GET, $sql_details, $table, $primaryKey, $columns )
+);
